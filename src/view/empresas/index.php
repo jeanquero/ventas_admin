aa482e68a311219d70b09a8a93afdf563d8c11c2
Jean quero <jeanquero@gmail.com>
2021-09-06 21:04:31 +0200

    init

---
+++ src/view/empresas/index.php
@@ -0,0 +1,6 @@
+<?php require_once  "/../common/header_title.php"; ?>             
+<main>
+                   hdhd 
+                </main>
+                <?php require_once  "../common/footer.php"; ?>
+               
