e499396bf6420fbc47ed8a9f13ea4501f3b116d4
unknown <lopezajoseg@gmail.com>
2021-09-18 22:06:57 -0400

    feat: ajustes en el menu

--- src/view/common/sidebar.php
+++ src/view/common/sidebar.php
@@ -1,7 +1,12 @@
+<?php
+$path = getcwd();
+?>
+
 <nav id="sidebar" class="">
   <div class="sidebar-header navbar-light border-bottom title-header" id="sidebar-title" style="background-color: #38414A; min-height: 90px; max-height: 90px">
       <span> <img src="../../../src/assets/img/logo.png"></span>
   </div>
+    <h6 class="color-activo p-2 mt-2">REGISTROS</h6>
   <ul class="list-unstyled components">
     <li>
       <a
@@ -12,9 +17,9 @@
         ><i class="fa fa-building" aria-hidden="true"></i> Empresas</a
       >
       <ul class="collapse list-unstyled" id="homeSubmenu">
-        <li><a href="./../empresas/contex.php" class="text-info" >Asociados Y colaboradores APA</a></li>
-        <li><a href="./../aven/contex.php" class="text-info">Empresas AVEM</a></li>
-        <li><a href="./../regular/contex.php" class="text-info">Regular</a></li>
+        <li><a href="./../empresas/contex.php" class="color-submenu" >Asociados Y colaboradores APA</a></li>
+        <li><a href="./../aven/contex.php" class="color-submenu">Empresas AVEM</a></li>
+        <li><a href="./../regular/contex.php" class="color-submenu">Regular</a></li>
       </ul>
     </li>
     <li>
@@ -26,22 +31,36 @@
         ><i class="fa fa-user" aria-hidden="true"></i> Personas</a
       >
       <ul class="collapse list-unstyled" id="homePersonas">
-        <li><a href="./../person/contex.php" class="text-info">Persona Natural</a></li>
-        <li><a href="./../estudiante/contex.php" class="text-info">Estudiante</a></li>
+        <li><a href="./../person/contex.php" class="color-submenu">Persona Natural</a></li>
+        <li><a href="./../estudiante/contex.php" class="color-submenu">Estudiante</a></li>
       </ul>
     </li>
   </ul>
   <div class="sidebar-header navbar-light" style="color: black;">
-    <div class="ml-2 p-2" ><a href="./../precios/contex.php" class="text-info">PPRECIO</a></div>
+    <div class="ml-1 p-2" ><a href="./../precios/contex.php" class="color-activo">PRECIO</a></div>
   </div>
 
   <div class="sidebar-header navbar-light" style="color: black;">
-    <div class="ml-2 p-2" >PAGOS</div>
+      <ul class="list-unstyled components">
+          <li>
+              <a
+                      href="#homePagos"
+                      data-toggle="collapse"
+                      aria-expanded="false"
+                      class="dropdown-toggle color-activo"
+              ></i> PAGOS</a
+              >
+              <ul class="collapse list-unstyled" id="homePagos">
+                  <li><a href="./../pago_por_empresas/contex.php" class="color-submenu">Empresas</a></li>
+                  <li><a href="./../pago_por_personas/contex.php" class="color-submenu">Personas</a></li>
+              </ul>
+          </li>
+      </ul>
   </div>
-  <ul class="list-unstyled">
+ <!-- <ul class="list-unstyled">
         <li><a href="./../pago_por_empresas/contex.php" class="text-info">Empresas</a></li>
         <li><a href="./../pago_por_personas/contex.php" class="text-info">Personas</a></li>
-      </ul>
+      </ul>-->
 </nav>
 <script>
  /*   $(document).ready(function(){
